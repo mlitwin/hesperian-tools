f88c259784f3bd221c3a7b64761045ee60758488
mlitwin <mlitwin@kcptech.com>
2011-09-11 08:45:01 -0700

    roll directory processor into this command - just make source a directory.

--- image_processing/image_convert.pl
+++ image_processing/image_convert.pl
@@ -4,13 +4,16 @@ use Getopt::Long;
 
 use File::Basename;
 use File::Path qw(make_path);
+use File::Find;
+
 
 my $convert = "convert";
 
-my ($resizeSize, $degrayAlpha);
+my ($resizeSize, $degrayAlpha, $wikidir);
 
 GetOptions("resizeSize:s", \$resizeSize,
-  "degrayAlpha:s", \$degrayAlpha);
+  "degrayAlpha:s", \$degrayAlpha,
+  "wikidir", \$wikidir);
 
 $resizeSize = $resizeSize || '800x800>';
 $degrayAlpha = $degrayAlpha || '.09';
@@ -30,14 +33,35 @@ my %Converts = (
 );
 
 die "Unknown command $Command" unless exists($Converts{$Command});
+my @cmd_base = ($convert, @{$Converts{$Command}});
 
-my @cmd = ($convert, @{$Converts{$Command}}, $Source, $Dest);
 
-print join(' ',@cmd), "\n";
+sub do_command {
+  my ($i, $o) = @_;
+  my @cmd = (@cmd_base, $i, $o);
+  print join(' ',@cmd), "\n";
 
-my $basedir = dirname($Dest);
-make_path($basedir);
+  my $basedir = dirname($o);
+  make_path($basedir);
 
-my $rc = system(@cmd);
-if ($rc & 127) { die "convert terminated" }
-return $rc;
+  my $rc = system(@cmd);
+  if ($rc & 127) { die "convert terminated" }
+  return $rc;
+}
+
+sub do_one_image {
+  return unless -f $_;
+  my $bn = basename($_);
+  return if $bn =~ /^\./;
+
+  my $destpath = $Dest . substr $_, length($Source);
+  do_command($_, $destpath);
+};
+
+if (-d $Source) {  
+  my @ImageDirs = $wikidir ?  map { "$Source/$_"} qw(0 1 2 3 4 5 6 7 8 9 a b c d e f): ($Source);
+
+  find({ wanted => \&do_one_image, no_chdir => 1 }, @ImageDirs);
+} else {
+  do_command($Source,$Dest);
+}
